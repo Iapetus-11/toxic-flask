1acac1d8d76840a711e515aa37b719e1c9ab785d
Milo Weinberg <iapetus011@gmail.com>
2021-08-28 14:25:33 -0400

    create analyze endpoint

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from detoxify import Detoxify
+from flask import Flask
+import classyjson as cj
+
+with open("config.json", "r") as config_file:
+    CONFIG = cj.load(config_file)
+
+detox_model = Detoxify("original")
+app = Flask(__name__)
+
+@app.route("/analyze/<str:text>")
+def analyze(text: str):
+    return {"toxicity": detox_model.predict(text)}
+
+if __name__ == "__main__":
+    app.run(host=CONFIG.host, port=CONFIG.port, debug=CONFIG.debug)
