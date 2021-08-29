427f3d661a48e9b29a0dfb985f22ef7d65d2be9d
Milo Weinberg <iapetus011@gmail.com>
2021-08-29 15:32:47 -0400

    add caching

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, abort
 from detoxify import Detoxify
 import classyjson as cj
+import functools
 
 with open("config.json", "r") as config_file:
     CONFIG = cj.load(config_file)
@@ -8,15 +9,18 @@ with open("config.json", "r") as config_file:
 detox_model = Detoxify("original")
 app = Flask(__name__)
 
+@functools.lru_cache(maxsize=CONFIG.max_cache_size)
+def analyze_text(text: str):
+    return {k: float(v) for k, v in detox_model.predict(text).items()}
 
 @app.route("/analyze/<string:text>")
-def analyze(text: str):
+def analyze_endpoint(text: str):
     auth = request.headers.get("authorization")
 
     if auth != CONFIG.auth:
         abort(401)
 
-    return {k: float(v) for k, v in detox_model.predict(text).items()}
+    return analyze_text(text)
 
 
 if __name__ == "__main__":
